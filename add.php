0f2414016f5e5b1c7498540c9c64b224db0b8544
Nikolay Andrievskiy <gazab@yandex.ru>
2017-11-30 22:42:32 +0300

    Валидация формы

--- add.php
+++ add.php
@@ -1,61 +1,6 @@
 <?php
 require_once ('functions.php');
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $form = $_POST;
-    $formError = [];
-    $newLot = [];
-
-    if ($form['lot-name'] != '') {
-        $lotName = esc($form['lot-name']);
-        $newLot['title'] = $lotName;
-    } else {
-       $formError[] = 'lot-name';
-    };
-    if ($form['category'] != '' && $form['category'] != 'Выберите категорию') {
-        $lotCategory = esc($form['category']);
-        $newLot['category'] = $lotCategory;
-    } else {
-        $formError[] = 'category';
-    };
-    if ($form['message'] != '') {
-        $lotMessage = esc($form['message']);
-        $newLot['message'] = $lotMessage;
-    } else {
-        $formError[] = 'message';
-    };
-    if ($form['lot-rate'] != '') {
-        $lotRate = esc($form['lot-rate']);
-        $newLot['price'] = $lotRate;
-    } else {
-        $formError[] = 'lot-rate';
-    };
-    if ($form['lot-step'] != '') {
-        $lotStep = esc($form['lot-step']);
-        $newLot['step'] = $lotStep;
-    } else {
-        $formError[] = 'lot-step';
-    };
-    if ($form['lot-date'] != '') {
-        $lotDate = esc($form['lot-date']);
-        $newLot['date'] = $lotDate;
-    } else {
-        $formError[] = 'lot-date';
-    };
-
-    if (empty($formError)) {
-        $lots[] = $newLot;
-        $count = count($lots)-1;
-        header('location: /lot.php?id='.$count);
-    } else {
-//        foreach ($formError as $item) {
-//            print $item;
-//        };
-//        var_dump($formError);
-        header('location: /add.php?'.$formError);
-    };
-};
-
 $content = renderTemplate('templates/add.php',['categories' => $categories]);
 $page = renderTemplate('templates/layout.php',['title' => $title, 'categories' => $categories, 'content' => $content]);
 
--- templates/add.php
+++ templates/add.php
@@ -1,35 +1,75 @@
+<?php
+require_once 'functions.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $form = $_POST;
+    $newLot = [];
+//    $error = [];
+
+    foreach ($form as $key => $item) {
+        if ($item != '' and $item != 'Выберите категорию') {
+            if ($key == 'lot-rate' or $key == 'lot-step') {
+                if (ctype_digit($item)) {
+                    $newLot[$key] = esc($item);
+                } else {
+                    $error[$key] = 'number';
+                };
+            } else {
+                $newLot[$key] = esc($item);
+            };
+        } else {
+            $error[$key] = '';
+        };
+    };
+    var_dump($_FILES);
+//    if (isset($_FILES)) {
+//
+//    } else {
+//        $error['file'] = '';
+//    };
+//    var_dump($error);
+//    var_dump($newLot);
+};
+
+
+?>
 <main>
     <nav class="nav">
         <ul class="nav__list container">
             <? foreach ($categories as $key => $value): ?>
-            <li class="nav__item">
-                <a href="all-lots.html"><?= $value['title'];?></a>
-            </li>
+                <li class="nav__item">
+                    <a href="all-lots.html"><?= $value['title']; ?></a>
+                </li>
             <? endforeach; ?>
         </ul>
     </nav>
-    <form class="form form--add-lot container<? if ($_GET['form'] == 'error'): ?><?= ' form--invalid';?><? endif; ?>" action="add.php" method="post" enctype="multipart/form-data"> <!-- form--invalid -->
+    <form class="form form--add-lot container<? if (isset($error)): ?><?= ' form--invalid'; ?><? endif; ?>"
+          action="add.php" method="post" enctype="multipart/form-data"> <!-- form--invalid -->
         <h2>Добавление лота</h2>
         <div class="form__container-two">
-            <div class="form__item<? if (isset($_GET['form'])): ?><?= ' form__item--invalid';?><? endif; ?>"> <!-- form__item--invalid -->
+            <div class="form__item<? if (isset($error['lot-name'])): ?><?= ' form__item--invalid'; ?><? endif; ?>">
+                <!-- form__item--invalid -->
                 <label for="lot-name">Наименование</label>
-                <input id="lot-name" type="text" name="lot-name" placeholder="Введите наименование лота">
+                <input id="lot-name" type="text" name="lot-name" placeholder="Введите наименование лота"
+                       value="<? if (isset($newLot['lot-name'])): ?><?= $newLot['lot-name']; ?><? endif; ?>">
                 <span class="form__error">Введите наименование лота</span>
             </div>
-            <div class="form__item<? if (isset($_GET['form'])): ?><?= ' form__item--invalid';?><? endif; ?>">
+            <div class="form__item<? if (isset($error['category'])): ?><?= ' form__item--invalid'; ?><? endif; ?>">
                 <label for="category">Категория</label>
-                <select id="category" name="category" required>
-                    <option>Выберите категорию</option>
+                <select id="category" name="category"
+                        value="<? if (isset($newLot['category'])): ?><?= $newLot['category']; ?><? endif; ?>">
+                    <option><? if (isset($newLot['category'])): ?><?= $newLot['category']; ?><? else: ?>Выберите категорию<? endif; ?></option>
                     <? foreach ($categories as $key => $value): ?>
-                    <option><?= $value['title'];?></option>
+                        <option><?= $value['title']; ?></option>
                     <? endforeach; ?>
                 </select>
                 <span class="form__error">Выберите категорию</span>
             </div>
         </div>
-        <div class="form__item form__item--wide<? if (isset($_GET['form'])): ?><?= ' form__item--invalid';?><? endif; ?>">
+        <div class="form__item form__item--wide<? if (isset($error['message'])): ?><?= ' form__item--invalid'; ?><? endif; ?>">
             <label for="message">Описание</label>
-            <textarea id="message" name="message" placeholder="Напишите описание лота"></textarea>
+            <textarea id="message" name="message"
+                      placeholder="Напишите описание лота"><? if (isset($newLot['message'])): ?><?= $newLot['message']; ?><? endif; ?></textarea>
             <span class="form__error">Напишите описание лота</span>
         </div>
         <div class="form__item form__item--file"> <!-- form__item--uploaded -->
@@ -47,24 +87,29 @@
                 </label>
             </div>
         </div>
-        <div class="form__container-three<? if (isset($_GET['form'])): ?><?= ' form__item--invalid';?><? endif; ?>">
+        <div class="form__container-three<? if (isset($error['lot-rate'])): ?><?= ' form__item--invalid'; ?><? endif; ?>">
             <div class="form__item form__item--small">
                 <label for="lot-rate">Начальная цена</label>
-                <input id="lot-rate" type="number" name="lot-rate" placeholder="0">
-                <span class="form__error">Введите начальную цену</span>
+                <input id="lot-rate" type="text" name="lot-rate" placeholder="0"
+                       value="<? if (isset($newLot['lot-rate'])): ?><?= $newLot['lot-rate']; ?><? endif; ?>">
+                <span class="form__error"><? if (isset($error['lot-rate']) and $error['lot-rate'] == 'number'): ?>Недопустимое значение<? else: ?>Введите начальную цену<? endif; ?></span>
             </div>
-            <div class="form__item form__item--small<? if (isset($_GET['form'])): ?><?= ' form__item--invalid';?><? endif; ?>">
+            <div class="form__item form__item--small<? if (isset($error['lot-step'])): ?><?= ' form__item--invalid'; ?><? endif; ?>">
                 <label for="lot-step">Шаг ставки</label>
-                <input id="lot-step" type="number" name="lot-step" placeholder="0">
-                <span class="form__error">Введите шаг ставки</span>
+                <input id="lot-step" type="text" name="lot-step" placeholder="0"
+                       value="<? if (isset($newLot['lot-step'])): ?><?= $newLot['lot-step']; ?><? endif; ?>">
+                <span class="form__error"><? if (isset($error['lot-rate']) and $error['lot-rate'] == 'number'): ?>Недопустимое значение<? else: ?>Введите шаг ставки<? endif; ?></span>
             </div>
-            <div class="form__item<? if (isset($_GET['form'])): ?><?= ' form__item--invalid';?><? endif; ?>">
+            <div class="form__item<? if (isset($error['lot-date'])): ?><?= ' form__item--invalid'; ?><? endif; ?>">
                 <label for="lot-date">Дата окончания торгов</label>
-                <input class="form__input-date" id="lot-date" type="date" name="lot-date">
+                <input class="form__input-date" id="lot-date" type="date" name="lot-date"
+                       value="<? if (isset($newLot['lot-date'])): ?><?= $newLot['lot-date']; ?><? endif; ?>">
                 <span class="form__error">Введите дату завершения торгов</span>
             </div>
         </div>
-        <span class="form__error form__error--bottom">Пожалуйста, исправьте ошибки в форме.</span>
+        <? if (isset($error)): ?>
+            <span class="form__error form__error--bottom">Пожалуйста, исправьте ошибки в форме.</span>
+        <? endif; ?>
         <button type="submit" class="button">Добавить лот</button>
     </form>
 </main>
